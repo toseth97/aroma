51e048a1e9f3c00a7733f720d12f1b09e2350e59
toseth97 <oluwasheun9721@gmail.com>
2024-02-06 12:52:52 +0100

    Update CSS for mobile navigation, hero section, and animations. Add Hero component.

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-    return <main className=""></main>;
+    return (
+        <main className="">
+            <Hero />
+        </main>
+    );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+import CartImage from "../assets/hero-banner.png.webp";
+
+const Hero = () => {
+    return (
+        <div className="flex w-full items-center justify-between hero__section">
+            <div className="hero__img h-full lg:w-5/12 w-1/12">
+                <Image
+                    src={CartImage}
+                    className="cartImage absolute lg:block hidden"
+                    alt="hero"
+                />
+            </div>
+            <div className="hero__text lg:w-7/12 w-11/12 h-full flex items-center flex-col  justify-center">
+                <div className="lg:w-7/12 w-11/12">
+                    <p className="text-2xl opacity-70 hero__p1">Shop is fun</p>
+                    <h1 className="font-bold text-5xl my-4 hero__h1">
+                        BROWSE OUR PREMIUM PRODUCT
+                    </h1>
+                    <p className="opacity-70 mb-8 hero__p2">
+                        Us which over of signs divide dominion deep fill bring
+                        they are meat beho upon own earth without morning over
+                        third. Their male dry. They are great appear whose land
+                        fly grass
+                    </p>
+
+                    <button className="browse-now__btn">Browse Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
