f698dfa16bf7e14e8e50da6ab3cbf71cdd5dda53
toseth97 <oluwasheun9721@gmail.com>
2024-02-06 17:52:34 +0100

    sticky navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={`overflow-x-hidden`}>
-                <header className="flex items-center justify-center shadow">
+                <header className="flex items-center justify-center shadow sticky top-0">
                     <Navigation />
                 </header>
                 <main className="">{children}</main>
