cda65b534d716fe420d83c37b0a5fddf3cc307fa
toseth97 <oluwasheun9721@gmail.com>
2024-05-04 15:10:17 +0100

    working on mobile filter section

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,70 +6,44 @@ import ProductCard from "./ProductCard";
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Product = () => {
-    const { products, productFilterBtn, setProductFilterBtn, category } =
-        useContext(ProductContext);
+    const {
+        productFilter,
+        setProductFilter,
+        category,
+        search,
+        handleSearch,
+        handleCategoryFilter,
+    } = useContext(ProductContext);
 
-    const cat = category.map((items) => items.title);
+    const cat = category && category.map((items) => items.title);
+    cat && cat.unshift("All");
 
     const filterDisplay = () => {
-        setProductFilterBtn(!productFilterBtn);
+        setProductFilter(!productFilter);
     };
     return (
-        <div className="w-full flex flex-col items-center justify-center ">
+        <div className="w-full flex flex-col items-center min-h-screen justify-center relative">
+            <div className="fixed bottom-10 object-center bg-blue-700 px-8 py-2 text-white z-20 rounded-3xl">
+                <p>Filter Products</p>
+            </div>
             <div className="w-full shop__hero flex flex-col items-center justify-center">
                 <h1 className="text-4xl title__text font-bold mb-2">
                     Shop Category
                 </h1>
                 <p className="opacity-60 text-sm">Home - Shop category</p>
             </div>
-            <div className="w-full sticky left-0 top-24 lg:hidden block z-20  mt-8 mb-3 ">
-                <p
-                    className="p-3 w-[50px] h-[50px] border border-gray-500 rounded bg-white ml-4"
-                    onClick={filterDisplay}
-                >
-                    <FaLongArrowAltRight
-                        style={{}}
-                        className="text-blue-700 text-3xl "
-                    />
-                </p>
-                <div
-                    className={`lg:w-3/12 w-9/12 gap-8 flex flex-col items-center justify-center lg:sticky absolute ease-in-out duration-300 lg:left-0  left-[-100%] lg:top-[80px] mt-4 ${
-                        productFilterBtn ? "left-2 bg-white py-8 px-2" : null
-                    } z-10`}
-                >
-                    <div className=" w-full flex flex-col items-center justify-center px-2">
-                        <div className="productNav w-full  pb-8 flex flex-col items-center justify-center">
-                            <h1 className=" productNav__title w-full text-white title__text flex items-center justify-center mb-8">
-                                Product Search
-                            </h1>
-                            <input
-                                type="text"
-                                placeholder="Search"
-                                className="inline-block w-11/12 py-1 px-2 text-sm outline-none opacity-70"
-                            />
-                            <div className="px-8"></div>
-                        </div>
-                    </div>
-                    <div className=" w-full flex flex-col items-center justify-center px-2">
-                        <div className="productNav w-full  pb-8">
-                            <h1 className=" productNav__title w-full text-white title__text flex items-center justify-center mb-8">
-                                Browse Categories
-                            </h1>
-                            <div className="px-8">
-                                {cat.map((item) => (
-                                    <p key={item} className="my-1 text-sm py-1">
-                                        {item}
-                                    </p>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
             <div className="w-11/12 bg-white lg:py-16 py-4 lg:px-8 px-2 flex items-start gap-8 z-10 relative">
+                {
+                    //******** mobile search *********/
+                }
+
                 <div
                     className={`lg:w-3/12 w-9/12 gap-8 flex flex-col items-center justify-center sticky lg:top-[80px] lg:block hidden z-10`}
                 >
+                    {
+                        //******** desktop search *********/
+                    }
                     <div className=" w-full flex flex-col items-center justify-center px-2">
                         <div className="productNav w-full  pb-8 flex flex-col items-center justify-center">
                             <h1 className=" productNav__title w-full text-white title__text flex items-center justify-center mb-8">
@@ -78,29 +52,39 @@ const Product = () => {
                             <input
                                 type="text"
                                 placeholder="Search"
+                                value={search}
+                                onChange={(e) => handleSearch(e)}
                                 className="inline-block w-11/12 py-1 px-2 text-sm outline-none opacity-70"
                             />
                             <div className="px-8"></div>
                         </div>
                     </div>
+
                     <div className=" w-full flex flex-col items-center justify-center px-2">
                         <div className="productNav w-full  pb-8">
                             <h1 className=" productNav__title w-full text-white title__text flex items-center justify-center mb-8">
                                 Browse Categories
                             </h1>
                             <div className="px-8">
-                                {cat.map((item) => (
-                                    <p key={item} className="my-1 py-1">
-                                        {item}
-                                    </p>
-                                ))}
+                                {category &&
+                                    cat.map((item) => (
+                                        <p
+                                            key={item}
+                                            className="my-2 py-1 w-full cursor-pointer "
+                                            onClick={() =>
+                                                handleCategoryFilter(item)
+                                            }
+                                        >
+                                            {item}
+                                        </p>
+                                    ))}
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="lg:w-10/12 w-full  flex flex-wrap gap-2  items-center justify-center border-t">
-                    {products ? (
-                        products.map((item) => (
+                    {productFilter ? (
+                        productFilter.map((item) => (
                             <ProductCard item={item} key={item.id} />
                         ))
                     ) : (
--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -1,6 +1,7 @@
 "use client";
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
+import { set } from "mongoose";
 
 const ProductContext = createContext();
 
@@ -9,9 +10,12 @@ export const ProductProvider = ({ children }) => {
     const [bestSeller, setBestSeller] = useState(null);
     const [category, setCategory] = useState(null);
     const [products, setProducts] = useState(null);
-    const [productFilterBtn, setProductFilterBtn] = useState(false);
+    const [productFilter, setProductFilter] = useState(false);
     const [productLoading, setProductLoading] = useState(false);
     const [categoryLoading, setCategoryLoading] = useState(false);
+    const [search, setSearch] = useState("");
+    const [categorySearch, setCategorySearch] = useState("All");
+
     //Load products from back end api
     useEffect(() => {
         setProductLoading((item) => true);
@@ -26,15 +30,48 @@ export const ProductProvider = ({ children }) => {
                     const [data1, data2] = data;
                     setBestSeller([data1[1], data1[12], data1[28]]);
                     setTrendingProduct([data1[0], data1[9], data1[2]]);
-                    console.log(data1);
-                    console.log(data2);
                     setCategory(data2.categories);
                     setProducts(data1);
+                    setProductFilter(data1);
                 })
                 .catch((error) => {
                     console.error(error);
                 });
     }, [productLoading]);
+
+    //set Search parameter
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
+    //using search parameter value to search for product
+    useEffect(() => {
+        if (search) {
+            const searchFilter = products.filter((item) =>
+                item.title.toLowerCase().includes(search.toLowerCase())
+            );
+            setProductFilter(searchFilter);
+        } else {
+            setProductFilter(products);
+        }
+    }, [search]);
+
+    //set category Parameter
+    const handleCategoryFilter = (params) => {
+        setCategorySearch(params);
+    };
+
+    //using category parameter value to filter product
+    useEffect(() => {
+        if (categorySearch === "All") {
+            setProductFilter(products);
+        } else {
+            const categoryFilter = products.filter(
+                (item) => item.category.title === categorySearch
+            );
+            setProductFilter(categoryFilter);
+        }
+    }, [categorySearch]);
 
     return (
         <ProductContext.Provider
@@ -42,9 +79,12 @@ export const ProductProvider = ({ children }) => {
                 trendingProduct,
                 bestSeller,
                 products,
-                productFilterBtn,
-                setProductFilterBtn,
+                productFilter,
+                setProductFilter,
                 category,
+                search,
+                handleSearch,
+                handleCategoryFilter,
             }}
         >
             {children}
