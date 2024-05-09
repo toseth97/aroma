d29325a5d507db97a31110a62ba63326a5785202
toseth97 <oluwasheun9721@gmail.com>
2024-05-09 14:43:25 +0100

    "Added state for detailed product, useEffect for fetching detailed product and Image component from next"
    This commit updates the DetailedProduct component by adding a state for the detailed product and a useEffect hook to fetch the detailed product based on

--- src/components/DetailedProduct.jsx
+++ src/components/DetailedProduct.jsx
@@ -1,23 +1,55 @@
 "use client";
 import React from "react";
-import { useContext } from "react";
+import Image from "next/image";
+import { useContext, useEffect, useState } from "react";
 import ProductContext from "@/context/ProductContext";
 import { useParams } from "next/navigation";
 
 const DetailedProduct = () => {
     const { products } = useContext(ProductContext);
+    const [detailedProduct, setDetailedProduct] = useState({});
     const { Id } = useParams();
-    const product =
-        products && products.filter((items) => items.id == Number(Id));
-    console.log(product);
+    console.log(Id);
+
+    // This useEffect is used because, there might be some cases where the user already have the url of the said products. In this way the Product Provider's product is empty. And if i filter it directly its gonna throw error. In need to pass the useEffect and Product dependency to ensure that it load product.
+    useEffect(() => {
+        const getProductDetail =
+            products && products.find((items) => items._id == Id);
+        setDetailedProduct(getProductDetail);
+    }, [Id, products]);
+
+    console.log(detailedProduct);
     return (
-        <>
-            {product && (
-                <div>
-                    <h1>{product.title}</h1>
+        <section className="w-full flex items-center justify-center py-16">
+            {detailedProduct ? (
+                <div className="flex flex-col lg:flex-row lg:justify-between justify-center w-11/12 lg:w-10/12 gap-8 ">
+                    <div className="lg:w-4/12 w-full rounded-lg">
+                        <Image
+                            alt={detailedProduct.title}
+                            src={detailedProduct.imageUrl}
+                            className="object-contain"
+                            width={400}
+                            height={400}
+                        />
+                    </div>
+                    <div className="lg:w-7/12 w-full">
+                        <h1 className="text-4xl font-bol title__text">
+                            {detailedProduct.title}
+                        </h1>
+                        <p className="text-sm my-8 opacity-80 text-justify">
+                            {detailedProduct.description}
+                        </p>
+                        <p className="font-semibold text-2xl title__text">
+                            ${detailedProduct.price}.00
+                        </p>
+                    </div>
+                </div>
+            ) : (
+                <div className="loading__screen h-52 flex items-center justify-center">
+                    <div className="loading"></div>
                 </div>
             )}
-        </>
+        </section>
     );
 };
 
