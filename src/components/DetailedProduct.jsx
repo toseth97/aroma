8317ddcf3d66a40f202b185e0bae5cfdb3efa4d7
toseth97 <oluwasheun9721@gmail.com>
2024-05-29 17:42:43 +0100

    Starting the review section of product

--- src/components/DetailedProduct.jsx
+++ src/components/DetailedProduct.jsx
@@ -44,68 +44,78 @@ const DetailedProduct = () => {
             >
                 <IoIosArrowRoundBack />
             </Link>
-            <div className="w-full flex items-center justify-center py-8">
+            <div className="w-full flex flex-col items-center justify-center py-8">
                 {" "}
                 {detailedProduct ? (
-                    <div className="flex flex-col lg:flex-row lg:justify-between justify-center w-11/12 lg:w-10/12 gap-8 ">
-                        <div className="lg:w-4/12 w-full rounded-lg">
-                            <Image
-                                alt={detailedProduct.title}
-                                src={detailedProduct.imageUrl}
-                                className="object-contain"
-                                width={400}
-                                height={400}
-                            />
-                        </div>
-                        <div className="lg:w-7/12 w-full">
-                            <h1 className="text-4xl font-bol title__text">
-                                {detailedProduct.title}
-                            </h1>
-                            <p className="text-sm my-4 opacity-80 text-justify">
-                                {detailedProduct.description}
-                            </p>
-                            <p className="font-semibold text-2xl title__text">
-                                ${detailedProduct.price}.00
-                            </p>
-                            <div className=" mt-4 flex lg:w-3/12 w-6/12  items-center justify-evenly  border rounded overflow-hidden ">
-                                <button
-                                    className="w-5/12 flex items-center justify-center grow-0 hover:bg-gray-200 active:bg-gray-200 duration-200 ease-in-out lg:py-3 lg:px-1 py-5 px-5"
-                                    onClick={() => orderQuantity("minus")}
-                                >
-                                    <FaMinus
-                                        style={{
-                                            color: "black",
-                                            fontSize: "14px",
-                                        }}
-                                    />
-                                </button>
-
-                                <p className="text-xl font-bold grow w-full text-center">
-                                    {quantity}
+                    <>
+                        <div className="flex flex-col lg:flex-row lg:justify-between justify-center w-11/12 lg:w-10/12 gap-8 ">
+                            <div className="lg:w-4/12 w-full rounded-lg">
+                                <Image
+                                    alt={detailedProduct.title}
+                                    src={detailedProduct.imageUrl}
+                                    className="object-contain"
+                                    width={400}
+                                    height={400}
+                                />
+                            </div>
+                            <div className="lg:w-7/12 w-full">
+                                <h1 className="text-4xl font-bol title__text">
+                                    {detailedProduct.title}
+                                </h1>
+                                <p className="text-sm my-4 opacity-80 text-justify">
+                                    {detailedProduct.description}
                                 </p>
+                                <p className="font-semibold text-2xl title__text">
+                                    ${detailedProduct.price}.00
+                                </p>
+                                <div className=" mt-4 flex lg:w-3/12 w-6/12  items-center justify-evenly  border rounded overflow-hidden ">
+                                    <button
+                                        className="w-5/12 flex items-center justify-center grow-0 hover:bg-gray-200 active:bg-gray-200 duration-200 ease-in-out lg:py-3 lg:px-1 py-5 px-5"
+                                        onClick={() => orderQuantity("minus")}
+                                    >
+                                        <FaMinus
+                                            style={{
+                                                color: "black",
+                                                fontSize: "14px",
+                                            }}
+                                        />
+                                    </button>
 
+                                    <p className="text-xl font-bold grow w-full text-center">
+                                        {quantity}
+                                    </p>
+
+                                    <button
+                                        className="w-5/12 flex items-center justify-center grow-0 hover:bg-gray-200 active:bg-gray-200 duration-200 ease-in-out lg:py-3 lg:px-1 py-5 px-5"
+                                        onClick={() => orderQuantity("plus")}
+                                    >
+                                        <FaPlus
+                                            style={{
+                                                color: "black",
+                                                fontSize: "14px",
+                                            }}
+                                        />
+                                    </button>
+                                </div>
                                 <button
-                                    className="w-5/12 flex items-center justify-center grow-0 hover:bg-gray-200 active:bg-gray-200 duration-200 ease-in-out lg:py-3 lg:px-1 py-5 px-5"
-                                    onClick={() => orderQuantity("plus")}
+                                    className="mt-6 bg-sky-900 lg:w-3/12 w-6/12 lg:py-2 py-3 rounded text-white duration-200 ease-in-out hover:bg-sky-800"
+                                    onClick={() =>
+                                        toast.success("successfully added")
+                                    }
                                 >
-                                    <FaPlus
-                                        style={{
-                                            color: "black",
-                                            fontSize: "14px",
-                                        }}
-                                    />
+                                    Add to Cart
                                 </button>
                             </div>
-                            <button
-                                className="mt-6 bg-sky-900 lg:w-3/12 w-6/12 lg:py-2 py-3 rounded text-white duration-200 ease-in-out hover:bg-sky-800"
-                                onClick={() =>
-                                    toast.success("successfully added")
-                                }
-                            >
-                                Add to Cart
-                            </button>
                         </div>
-                    </div>
+
+                        <div className="w-full my-4 mt-8 flex flex-col items-center justify-center bg-sky-100">
+                            <div className="  w-full flex items-center justify-center">
+                                <h2 className="px-16 py-2 my-2 bg-white font-semibold">
+                                    Reviews
+                                </h2>
+                            </div>
+                        </div>
+                    </>
                 ) : (
                     <div className="loading__screen h-52 flex items-center justify-center">
                         <div className="loading"></div>
