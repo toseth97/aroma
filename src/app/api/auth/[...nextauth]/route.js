e70f6a9b24acc49c268ae909bf6043c74a58cf3d
toseth97 <oluwasheun9721@gmail.com>
2024-05-31 22:18:27 +0100

    "Refactor user creation and update logic in NextAuth Google provider callback"

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,14 +2,14 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import User from "@/model/userSchema";
 import connectDB from "@/lib/connectDB";
-
 const handler = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callback: async (profile, tokens) => {
-                // Create a new local user document
+                await connectDB(); // Wait for the database connection to be established
+
                 const user = {
                     name: profile.name,
                     email: profile.email,
@@ -17,26 +17,20 @@ const handler = NextAuth({
                     googleId: profile.sub,
                 };
 
-                (async () => {
-                    await connectDB();
-                    console.log("code ran");
-                    const existingUser = await User.find({
-                        email: user.email,
-                    });
-                    const localUser =
-                        !existingUser &&
-                        new User({
-                            name: user.name,
-                            email: user.email,
-                            image: user.image,
-                        });
+                const existingUser = await User.findOne({ email: user.email });
+                let localUser;
 
+                if (!existingUser) {
+                    // Create a new local user document
+                    localUser = new User(user);
                     try {
                         await localUser.save();
                     } catch (err) {
                         console.log(err.message);
                     }
-                })();
+                }
+
+                return localUser;
             },
         }),
     ],
