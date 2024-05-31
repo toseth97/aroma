e2e3e106284591f03a163d590d63df17c6cc4a4e
toseth97 <oluwasheun9721@gmail.com>
2024-05-31 18:58:58 +0100

    Remove console log statement for detailedProduct.ratings in DetailedProduct component.

--- src/components/DetailedProduct.jsx
+++ src/components/DetailedProduct.jsx
@@ -37,8 +37,6 @@ const DetailedProduct = () => {
         }
     }
 
-    console.log(detailedProduct.ratings);
-
     return (
         <section className="w-full py-4 px-8">
             <ToastContainer />
